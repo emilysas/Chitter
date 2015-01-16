5ea820cc564b364dd9315cf5dec52ee62dcdd2d1
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-16 20:16:32 +0000

    testing for unique usernames

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -10,12 +10,17 @@ feature "sign up"  do
     # passwords are secured with bcrypt
 
   scenario "a user can sign up" do
-    visit '/'
-    click_on 'Sign Up'
     expect{ sign_up('test', 'username', 'test@test.com', '1234', '1234') }.to change(User, :count).by(1)
   end
 
+  scenario "a user cannot sign up with an existing username" do
+    sign_up('test', 'username', 'test@test.com', '1234', '1234')
+    expect{ sign_up('another_test', 'username', 'another_test@test.com', '4321', '4321') }.not_to change(User, :count)
+  end
+
   def sign_up(name, username, email, password, password_confirmation)
+    visit '/'
+    click_on 'Sign Up'
     fill_in 'name', :with => name
     fill_in 'username', :with => username
     fill_in 'email', :with => email
